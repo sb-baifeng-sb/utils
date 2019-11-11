e5b5db2a991b8b8b1e5c04a88382a461e3c7836c
baifeng <qq394811161@gmail.com>
2019-11-11 10:32:35 +0800

    first commit

---
+++ Singleton.h
@@ -0,0 +1,33 @@
+
+#ifndef __ART_SINGLETON_H__
+#define __ART_SINGLETON_H__
+
+namespace utils {
+
+template <typename T>
+class Singleton {
+public:
+    virtual ~Singleton() {}
+public:
+    static T* Shared() {
+        if (_instance == nullptr) {
+            _instance = new T;
+        }
+        return _instance;
+    }
+    static void Destroy() {
+        if (_instance != nullptr) {
+            delete _instance;
+            _instance = nullptr;
+        }
+    }
+private:
+    static T* _instance;
+};
+
+template <typename T>
+T* Singleton<T>::_instance = nullptr;
+
+}
+
+#endif // __ART_SINGLETON_H__
